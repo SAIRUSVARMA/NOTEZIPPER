23549492cacc15d74882ff520268d6b4f2982435
SAIRUS VARMA <sairusvarma201234@gmail.com>
2022-12-03 13:19:13 +0530

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import LandingPage from "./screens/LandingPage/LandingPage";
+import MyNotes from "./screens/MyNotes/MyNotes";
+import { Routes, Route } from "react-router-dom";
 
-const App = () => <h1 className="App">Hello World!!! </h1>;
-  
+const App = () => (
+  <Routes>
+    <Header />
+    <main>
+      <Route path="/" element={LandingPage} exact />
+      <Route path="/mynotes" element={<MyNotes />} exact />
+    </main>
 
+    <Footer />
+  </Routes>
+);
 
 export default App;
