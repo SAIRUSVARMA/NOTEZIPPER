94478ec9d217f30703d5a5d45b5dcbd3cce8f828
SAIRUS VARMA <sairusvarma201234@gmail.com>
2022-12-03 15:56:33 +0530

    EDIT1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,21 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
 import MyNotes from "./screens/MyNotes/MyNotes";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => (
-  <Routes>
-    <Header />
-    <main>
-      <Route path="/" element={LandingPage} exact />
-      <Route path="/mynotes" element={<MyNotes />} exact />
-    </main>
+  <BrowserRouter>
+      <Header />
+      <main>
+       <Routes>
+        <Route path="/" element={<LandingPage/>} exact />
+        <Route path="/mynotes" element={<MyNotes />} exact />
+       </Routes>
+       </main>
 
-    <Footer />
-  </Routes>
+      <Footer />
+    
+  </BrowserRouter>
 );
 
 export default App;
