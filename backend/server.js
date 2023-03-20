f276c64eb412ddbd75fc3b3adf26d1e438e247e5
SAIRUS VARMA <sairusvarma201234@gmail.com>
2023-03-20 18:37:34 +0530

    EDIT4

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -16,6 +17,20 @@ app.use("/api/notes", noteRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
